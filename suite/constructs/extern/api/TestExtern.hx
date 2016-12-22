7f3bddd22e5b8cbc349a81e5ed53731fc94eeba8
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2016-12-22 00:19:47 -0500

    Add non-passing test from TestExtern to suite.
    The inlines do not get emitted. I assume that they are supposed
    to be. If not, the test needs to be fixed and the rationale behind
    this behavior documented.

---
+++ suite/constructs/extern/api/TestExtern.hx
@@ -0,0 +1,8 @@
+extern class TestExtern
+{
+	@:overload(function(src:String, options:Int) : String {})
+	static function play(src:String, ?interrupt:String, ?delay:Int, ?offset:Int, ?loop:Int, ?volume:Float, ?pan:Float, ?startTime:Int, ?duration:Int) : String;
+	
+	inline function myInlineFunc() : Void { }
+	static inline function myStaticInlineFunc() : Void { }
+}
